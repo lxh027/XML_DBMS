920dd799c1fc87a466c524d341d731afebcae6b6
lxh027 <992874705@qq.com>
2021-06-29 08:14:15 +0800

    feat: 添加一些常量

---
+++ constants/code.go
@@ -0,0 +1,18 @@
+package constants
+
+const (
+	CodeSuccess =  0
+	CodeError = 1
+)
+
+const (
+	AuthOK = 0
+	AuthError = 1
+)
+
+const (
+	SqlOK = 0
+	SqlSyntaxError = 1
+	SqlError = 2
+	Undefined = 3
+)
