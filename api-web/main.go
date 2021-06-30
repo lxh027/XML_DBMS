4ab9d6fb4d3d05330a0a815cdad87ddecd52b7ff
lxh027 <992874705@qq.com>
2021-06-30 22:24:38 +0800

    fix: grpc包名错误

--- api-web/main.go
+++ api-web/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"log"
-	"lxh027.com/xml-dbms/api-web/grpc"
+	"lxh027.com/xml-dbms/api-web/grpc_client"
 	"lxh027.com/xml-dbms/api-web/server"
 )
 func main() {
 	// Test Rpc Conn
-	if err := grpc_server.ExecTestConn(context.Background()); err != nil {
+	if err := grpc_client.ExecTestConn(context.Background()); err != nil {
 		log.Printf("Fail to open rpc conn: %v\n", err.Error())
 	}
 	// 启动httpServer
