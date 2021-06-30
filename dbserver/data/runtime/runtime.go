4492f0f0df7845e69b0795492be6289764cb0a76
lxh027 <992874705@qq.com>
2021-06-30 21:12:13 +0800

    fix: 设置runtime数据为包外可访问

--- dbserver/data/runtime/runtime.go
+++ dbserver/data/runtime/runtime.go
@@ -8,9 +8,9 @@ import (
 	"lxh027.com/xml-dbms/dbserver/data/model"
 )
 
-var server *model.Server
-var databases map[string]*model.DataBase
-var tables map[string]*model.AllData
+var Server *model.Server
+var Databases map[string]*model.DataBase
+var Tables map[string]*model.AllData
 //var indexes map[string]*model.Indexes
 
 func LoadDataFromXML()  {
@@ -20,15 +20,15 @@ func LoadDataFromXML()  {
 	if err != nil {
 		log.Panicf(err.Error())
 	}
-	err = xml.Unmarshal(xmlData, server)
+	err = xml.Unmarshal(xmlData, Server)
 	if err != nil {
 		log.Panicf(err.Error())
 	}
-	log.Printf("加载根信息成功: %v", server)
-	databases = make(map[string]*model.DataBase)
-	tables = make(map[string]*model.AllData)
+	log.Printf("加载根信息成功: %v", Server)
+	Databases = make(map[string]*model.DataBase)
+	Tables = make(map[string]*model.AllData)
 	//indexes = make(map[string]*model.Indexes)
-	for _, databaseInfo := range server.DataBases {
+	for _, databaseInfo := range Server.DataBases {
 		xmlData, err := ioutil.ReadFile(databaseInfo.Location)
 		if err != nil {
 			log.Panicf(err.Error())
@@ -39,7 +39,7 @@ func LoadDataFromXML()  {
 			log.Panicf(err.Error())
 		}
 		log.Printf("加载数据库信息成功: %v", database)
-		databases[databaseInfo.Name] = &database
+		Databases[databaseInfo.Name] = &database
 		// 加载表信息
 		for _, tableInfo := range database.Tables {
 			xmlData, err := ioutil.ReadFile(tableInfo.Location)
@@ -52,7 +52,7 @@ func LoadDataFromXML()  {
 				log.Panicf(err.Error())
 			}
 			log.Printf("加载数据信息成功: %v", data)
-			tables[databaseInfo.Name+"."+tableInfo.Name] = &data
+			Tables[databaseInfo.Name+"."+tableInfo.Name] = &data
 			// 构造索引
 			//indexes[databaseInfo.Name+"."+tableInfo.Name] = buildIndex(databaseInfo.Name, &tableInfo)
 		}
@@ -66,7 +66,7 @@ func buildIndex(databaseName string, table *model.Table) *model.Indexes  {
 		for indexId, column := range table.Columns {
 			if index.Name == column.Name {
 				tableIndex := make(map[string]int)
-				for i, row := range tables[databaseName+"."+table.Name].Rows {
+				for i, row := range Tables[databaseName+"."+table.Name].Rows {
 					tableIndex[row.Data[indexId]] = i
 				}
 				indexes = append(indexes, model.Index{Name: index.Name, IndexMap: tableIndex})
