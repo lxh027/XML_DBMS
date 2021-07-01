b8d9d09941672a388c7ac5ed6036729a92023907
lxh027 <992874705@qq.com>
2021-07-02 00:04:08 +0800

    fix: web路由

--- api-web/routes/router.go
+++ api-web/routes/router.go
@@ -12,5 +12,5 @@ func Routes(router *gin.Engine)  {
 
 	router.POST("/exeSql", controller.ExecSql)
 
-	router.StaticFS("/sql/", http.Dir("./web"))
+	router.StaticFS("/sql/", http.Dir("./api-web/web"))
 }
